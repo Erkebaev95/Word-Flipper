2ae05976bc85e0bcfb2a85fc66863c0fcf5d8b38
Erkebaev Nursultan <erkebaevnursultan@gmail.com>
2021-02-07 15:40:19 +0600

    practice exercise

---
+++ src/Exercise.java
@@ -0,0 +1,21 @@
+public class Exercise {
+    public static void main(String[] args) {
+        String[] a = {"Каждый ", " охотник ", " желает ", " знать ", " где ", " сидит ", " фазан "};
+
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i]);
+        }
+        int n = a.length;
+        String temp;
+
+        for (int i = 0; i < n / 2; i++) {
+            temp = a[n - i - 1];
+            a[n - i - 1] = a[i];
+            a[i] = temp;
+        }
+
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i]);
+        }
+    }
+}
